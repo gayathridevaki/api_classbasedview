4535e75332336d1edd23269ebaaa909acbbb43c4
sai prakash <prakashsaivenkat@gmail.com>
2024-03-05 17:42:27 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -5,12 +5,13 @@ from app.serializers import *
 
 # Create your views here.
 class productcrud(APIView):
-    def get(self,request):
+    def get(self,request,id):
         PDO=product.objects.all()
+        #PDO=product.objects.get(id=id)
         PJO=productmodelserializer(PDO,many=True)
         return Response(PJO.data)
     
-    def post(self,request):
+    def post(self,request,id):
         JOD=request.data#json data
         PDO=productmodelserializer(data=JOD)#orm data
         if PDO.is_valid():
@@ -18,3 +19,29 @@ class productcrud(APIView):
             return Response({'insert':'data inserted successfully'})
         else:
             return Response({'error':'data is not inserted successfully'})
+        
+    def put(self,request,id):
+        PO=product.objects.get(id=id)
+        UPDO=productmodelserializer(PO,data=request.data)
+        if UPDO.is_valid():
+            UPDO.save()
+            return Response({'update':'data is updated'})
+        else:
+            return Response({'error':'data is not updated'})
+        
+
+    def patch(self,request,id):
+        PO=product.objects.get(id=id)
+        UPDO=productmodelserializer(PO,data=request.data,partial=True)
+        if UPDO.is_valid():
+            UPDO.save()
+            return Response({'update':'data is updated'})
+        else:
+            return Response({'error':'data is not updated'})
+        
+    def delete(self,request,id):
+        product.objects.get(id=id).delete()
+        return Response({'data':'data is deleted'})
+
+        
+    
--- pjt2/urls.py
+++ pjt2/urls.py
@@ -20,5 +20,5 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('productcrud/',productcrud.as_view(),name='productcrud')
+    path('productcrud/<id>/',productcrud.as_view(),name='productcrud')
 ]
